919e9b9e2ce40efac703b6244fd8ae36672707a5
Kevin Pham <keevan.pham@gmail.com>
2021-11-10 16:53:30 +1100

    Fix codechecker issues and warnings

--- classes/check/enabled.php
+++ classes/check/enabled.php
@@ -23,6 +23,7 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 namespace tool_forcedcache\check;
+defined('MOODLE_INTERNAL') || die();
 use core\check\check,
     core\check\result;
 
